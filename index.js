f09f8042d255b88d6ac80753b9e1d55519285557
skarimo <karimovj@gmail.com>
2018-09-23 13:53:55 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,77 @@
+let driverId = 0
+let passengerId = 0
+let tripID = 0
+let store = {drivers: [], passengers: [], trips: []}
+
+  class Driver {
+    constructor(name) {
+    this.name = name
+    this.id = driverId++
+
+    store.drivers.push(this);
+    }
+
+    trips() {
+      return store.trips.filter(trip => {
+        return trip.driverId == this.id;
+      });
+    }
+    passengers() {
+      return this.trips().map(trip => {
+        return trip.passenger();
+      });
+    }
+
+  }
+
+  class Passenger {
+    constructor(name, userId) {
+    this.name = name
+    this.id = passengerId++
+    this.userId = userId
+
+    store.passengers.push(this);
+    }
+    trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId == this.id;
+      });
+    }
+    drivers() {
+      return this.trips().map(trip => {
+        return trip.driver();
+      });
+    }
+
+  }
+
+  class Trip {
+    constructor(driver, passenger) {
+      this.id = tripID++
+      this.driverId = driver.id
+      this.passengerId = passenger.id
+      // this.passenger = passenger
+
+      store.trips.push(this);
+    }
+
+    passenger(){
+      return store.passengers.find(passenger => {
+        return passenger.id === this.passengerId;
+      });
+    }
+    driver(){
+      return store.drivers.find(driver => {
+        return driver.id === this.driverId;
+      });
+    }
+  }
+
+
+
+
+
+
+
+
+//
